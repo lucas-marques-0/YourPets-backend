905c495a34c24773fc1281ef8c1bddf0568502c8
Lucas Marques <lucas.marques@gauge.haus>
2024-06-13 15:52:25 -0300

    atualizando

--- database-postgres.js
+++ database-postgres.js
@@ -1,18 +1,22 @@
-import { randomUUID } from "crypto"
-import { sql } from "./db.js"
+import { randomUUID } from "crypto";
+import { sql } from "./db.js";
 
 export class DatabasePostgres {
-    async criarUsuario(usuario) {
-        const usuarioId = randomUUID()
-        const { username, email, password, pets } = usuario
-        await sql `insert into usuarios (id, username, email, password, pets) VALUES (${usuarioId}, ${username}, ${email}, ${password}, ${pets})`
-    }
+  async criarUsuario(usuario) {
+    const usuarioId = randomUUID();
+    const { username, email, password, pets } = usuario;
+    await sql`insert into usuarios (id, username, email, password, pets) VALUES (${usuarioId}, ${username}, ${email}, ${password}, ${pets})`;
+  }
 
-    async getUser(userID) {
-        return await sql `select * from usuarios where id = ${userID}`
-    }
+  async getUsers() {
+    return await sql`select * from usuarios`;
+  }
 
-    async updateUserPets(id, pets) {
-        await sql `UPDATE usuarios SET pets = ${pets} WHERE id = ${id}`
-    }
+  async getUser(userID) {
+    return await sql`select * from usuarios where id = ${userID}`;
+  }
+
+  async updateUserPets(id, pets) {
+    await sql`UPDATE usuarios SET pets = ${pets} WHERE id = ${id}`;
+  }
 }
--- server.js
+++ server.js
@@ -1,62 +1,77 @@
-import express from 'express';
-import cors from 'cors'
-import jwt from 'jsonwebtoken';
-import { DatabasePostgres } from './database-postgres.js';
+import express from "express";
+import cors from "cors";
+import jwt from "jsonwebtoken";
+import { DatabasePostgres } from "./database-postgres.js";
 
 const app = express();
 const port = process.env.PORT || 3333;
 const database = new DatabasePostgres();
 
-app.use(cors({
-  origin: '*',
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(express.json());
 
 const authenticateToken = (req, res, next) => {
   const { token } = req.body;
-  if (!token) return res.status(401).json({ message: 'Não achou o token.' })
-  const verifyToken = jwt.verify(token, 'segredo-do-jwt');
-  if (!verifyToken) return res.status(404).json({ message: 'Token inválido.' })
+  if (!token) return res.status(401).json({ message: "Não achou o token." });
+  const verifyToken = jwt.verify(token, "segredo-do-jwt");
+  if (!verifyToken) return res.status(404).json({ message: "Token inválido." });
   next();
-}
+};
 
-app.post('/usuarios', async (req, res) => {
+app.post("/usuarios", async (req, res) => {
   const { action } = req.body;
-  if (action === 'cadastro') {
+  if (action === "cadastro") {
     const { username, email, password, pets } = req.body;
     await database.criarUsuario({
       username: username,
       email: email,
       password: password,
-      pets: pets
+      pets: pets,
     });
     return res.status(201).send();
   }
-  if (action === 'login') {
+  if (action === "login") {
     const { userID, password } = req.body;
     const userInfo = await database.buscarUsuarioID(userID);
     const userPassword = userInfo[0].password;
     if (userPassword === password) {
-      const token = jwt.sign({ id: userInfo.id, email: userInfo.email }, 'segredo-do-jwt', {
-        expiresIn: '1d',
-      });
+      const token = jwt.sign(
+        { id: userInfo.id, email: userInfo.email },
+        "segredo-do-jwt",
+        {
+          expiresIn: "1d",
+        }
+      );
       const userObject = { ...userInfo[0], password: undefined };
       return res.status(201).json({ token, user: userObject });
     } else {
-      return res.status(401).json({ error: 'Credenciais inválidas.' });
+      return res.status(401).json({ error: "Credenciais inválidas." });
     }
   }
 });
 
-app.post('/usuarios/:id', authenticateToken, async (req, res) => {
+app.get("/usuarios", async (req, res) => {
+  const users = await database.getUsers();
+  const userObjects = users.map((user) => {
+    const { password, ...userObject } = user;
+    return userObject;
+  });
+  return res.json(userObjects);
+});
+
+app.post("/usuarios/:id", authenticateToken, async (req, res) => {
   const userID = req.params.id;
   const userInfos = await database.getUser(userID);
   const userObject = { ...userInfos[0], password: undefined };
   return res.status(201).json(userObject);
 });
 
-app.put('/usuarios/:id', authenticateToken, async (req, res) => {
+app.put("/usuarios/:id", authenticateToken, async (req, res) => {
   const { userID, pets } = req.body;
   await database.updateUserPets(userID, pets);
   return res.status(201).send();
